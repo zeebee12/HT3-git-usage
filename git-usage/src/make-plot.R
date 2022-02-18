46c313beb53d4919670afa5b2463f8140f61d387
zeebee12 <87305447+zeebee12@users.noreply.github.com>
2022-02-18 16:47:16 +0530

    Update make-plot.R

--- git-usage/src/make-plot.R
+++ git-usage/src/make-plot.R
@@ -6,16 +6,17 @@ g <- ggplot(
   data = sods_data,
   mapping = aes(x = year, y = percentage)) +
   geom_point() +
-  geom_smooth(method = "lm") +
+  geom_smooth(method = "lm", colour = "darkgrey") +
   geom_text(
     aes(x = 2020, y = 82.8, label = "only GitHub"),
+    size = 3,
     nudge_x = 0.2,
-    nudge_y = -4) +
+    nudge_y = -6) +
   labs(
     x = "Year",
-    y = "Percentage who used git",
-    title = "Git usage has increased",
-    subtitle = "Data from Stackoverflow Developer Survey")
+    y = "Percentage who used git") +
+  ylim(c(0,100)) +
+  theme_bw()
 
 ggsave(filename = "out/git-usage.png",
        plot = g,
